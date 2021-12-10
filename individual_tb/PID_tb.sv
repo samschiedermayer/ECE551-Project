ffde9b7b06dc1840397e682e63ce1d6afded452d
John Wirth <jjwirth2@wisc.edu>
2021-12-10 16:11:50 -0600

    Adjusted pipelining in PID

--- individual_tb/PID_tb.sv
+++ individual_tb/PID_tb.sv
@@ -1,94 +1,72 @@
-module PID_tb();
+`timescale 1ns/1ps
+module PID_tb;
 
-///////////////////////////////////////////////////////////////
-// Declare signals                                          //
-/////////////////////////////////////////////////////////////
 logic clk, rst_n, moving, err_vld;
-logic signed [11:0] error;
-logic [9:0] frwrd;
-logic [10:0] lft_spd, rght_spd;
-reg [24:0] stim[0:1999]; // 25-bit wide 2000 entry ROM
-reg [21:0] resp[0:1999]; // 22-bit wide 2000 entry ROM
 
-///////////////////////////////////////////////////////////////
-// Instantiate the DUT                                      //
-/////////////////////////////////////////////////////////////
-PID DUT(.clk(clk), .rst_n(rst_n), .moving(moving), .err_vld(err_vld),
-.error(error), .frwrd(frwrd), .lft_spd(lft_spd), .rght_spd(rght_spd));
+logic [11:0] error;
+logic  [9:0] frwrd;
+logic [10:0] lft_spd, rght_spd, exp_lft, exp_rght;
 
-///////////////////////////////////////////////////////////
-// Task for waiting for signals or timing out the test  //
-/////////////////////////////////////////////////////////
-// task automatic wait_for_sig(ref sig, input int clks_to_wait, input int pos);
-//   fork
-//     begin : timeout
-//       repeat(clks_to_wait) @(posedge clk);
-//       $display("TEST FAILED");
-//       $display("ERROR: timed out waiting for signal, %d clock cycles", clks_to_wait);
-//       $stop();
-//     end
-//     begin
-//       if(pos === 1) begin
-//         $display("Waiting for posedge");
-//         @(posedge sig);
-//       end
-//       else begin
-//         $display("Waiting for negedge");
-//         @(negedge sig);
-//       end
-//       disable timeout;
-//     end
-//   join
-// endtask
+PID iDut(.clk(clk),.rst_n(rst_n),.moving(moving),.err_vld(err_vld),
+	.error(error),.frwrd(frwrd),.lft_spd(lft_spd),.rght_spd(rght_spd));
 
-///////////////////////////////////////////////////////////////
-// Testing Logic                                            //
-/////////////////////////////////////////////////////////////
+reg [24:0] stim[0:1999];
+reg [21:0] resp[0:1999];
+
+logic checking_val;
+
+logic tb_err;
 initial begin
-  ///////////////////////////////////////////////////////////////
-  // Default values and DUT reset                             //
-  /////////////////////////////////////////////////////////////
-    $readmemh("PID_stim.hex", stim);
-    $readmemh("PID_resp.hex", resp);
-    clk = 0;
-    @(posedge clk);
-    // rst_n = 0;
-    // @(posedge clk);
-    // @(negedge clk);
-    // rst_n = 1;
+  tb_err = 0;
 
-    ///////////////////////////////////////////////////////////////
-    // Stimulus Testing                                         //
-    /////////////////////////////////////////////////////////////
-    $display("Executing Stimulus Testing");
+  checking_val = 0;
 
-    foreach (stim[i])begin
-      rst_n = stim[i][24];
-      moving = stim[i][23];
-      err_vld = stim[i][22];
-      error = stim[i][21:10];
-      frwrd = stim[i][9:0];      
-      repeat (10) @(negedge clk);
+  // initialize values
+  clk = 0;
+  rst_n = 1;
+  moving = 0;
+  err_vld = 0;
+  error = 0;
+  frwrd = 0;
 
-      if(lft_spd !== resp[i][21:11] && rght_spd !== resp[i][10:0]) begin
-        $display("i: %h", i);
+  // read in vectors
+  $readmemh("PID_stim.hex",stim);
+  $readmemh("PID_resp.hex",resp);
 
-        $display("Observed lft_spd: %h", lft_spd);
-        $display("Expected lft_spd: %h", resp[i][21:11]);
+  // loop through the vectors and check input vs output
+  for (int i = 0; i < 2000; i++) begin
+    rst_n   = stim[i][24];
+    moving  = stim[i][23];
+    err_vld = stim[i][22];
+    error   = stim[i][21:10];
+    frwrd   = stim[i][ 9: 0];
 
-        $display("Observed rght_spd: %h", rght_spd);
-        $display("Expected rght_spd: %h", resp[i][10:0]);
+    exp_lft  = resp[i][21:11];
+    exp_rght = resp[i][10: 0];
+    repeat (3) @(negedge clk);
+    checking_val = 1;
+    #1;
+    if (lft_spd !== exp_lft) begin
+      $display("Error, lft_spd does not match for input %d.  Expected: %0h, Actual: %0h",i,exp_lft,lft_spd);
+      tb_err = 1;
+      $display("Test %d",i);
+      // $stop();
+    end
+    if (rght_spd !== exp_rght) begin
+      $display("Error, rght_spd does not match for input %d. Expected: %0h, Actual: %0h",i,exp_rght,rght_spd);
+      tb_err = 1;
+      // $stop();
+    end
+    #1;
+    checking_val = 0;
+  end
 
-        $display("Tests Failed!");
-        $stop();
-      end
-    end  
-    
-    $display("Yahoo, stimulus testing completed successfully!");
-    $stop();
+  if (0 === tb_err)
+    $display("Yahoo! All tests passed :)");
+  $stop();
 end
 
-always 
-    #10 clk <= ~clk;
+always #5 clk = ~clk;
 
 endmodule
+
